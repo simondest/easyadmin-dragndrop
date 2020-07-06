2cbe42b724717857ed1c29df641d9f365f56d5e4
Simon Destombes <simon@vercors-net.com>
2020-07-06 20:38:42 +0200

    Update DefaultController.php

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -33,11 +33,11 @@ class DefaultController extends AbstractController
     {
         $entityClassNameArray = explode("\\", $entityClass);
         $entityClassName = end($entityClassNameArray);
-        try {
+        /*try {
             $rc = new ReflectionClass($entityClass);
         } catch (\ReflectionException $error) {
             throw new \ReflectionException("The class name " . $entityClass . "  cannot be reflected.");
-        }
+        }*/
         
         $em = $this->getDoctrine()->getManager();
         $e = $em->getRepository($rc->getName())
@@ -53,7 +53,7 @@ class DefaultController extends AbstractController
             "entity" => $entityClassName,
             "sortField" => "position",
             "sortDirection" => "ASC",
-            "filters" => json_decode($request->query->get('filters'))
+            //"filters" => json_decode($request->query->get('filters'))
         ));
     }
 }
